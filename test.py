9129cba86ad0e2ef07f5d434f8d3445271744b62
Uiyun Kim <uiyun_kim@student.uml.edu>
2020-10-04 00:47:55 +0900

    updated gitignore

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
 import scipy.io
 import matplotlib.pyplot as plt
 
-mat = scipy.io.loadmat('set_b/BVPr.mat')
+mat = scipy.io.loadmat('set_a/day1.mat')
 
 print(list(mat.keys()))
 
-data = mat['BVPr_37']
+data = mat['day1']
 
 print(data.shape)
-data = data.reshape((1,31983))
+data = data.reshape((32,2001))
 plt.plot(data[0])
 plt.show()
 print(type(mat))
