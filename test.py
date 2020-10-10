01a707b2bc7ae7e4c2cb458bc2bc017e9e4cd1d0
Uiyun Kim <uiyun_kim@student.uml.edu>
2020-10-10 12:41:18 +0900

    update

--- test.py
+++ test.py
@@ -1,16 +1,53 @@
 import scipy.io
 import matplotlib.pyplot as plt
+import math
 
-mat = scipy.io.loadmat('set_a/day1.mat')
+list_data = []
+for d in range(1,21):
+    day = 'day' + str(d)
+    mat = scipy.io.loadmat('set_a/' + day+'.mat')
 
-print(list(mat.keys()))
+    data = mat[day]
 
-data = mat['day1']
+    data = data.reshape((32,2001))
 
-print(data.shape)
-data = data.reshape((32,2001))
-plt.plot(data[0])
-plt.show()
-print(type(mat))
 
-print(mat)
+    for i in range(1,len(data)+1):
+
+        if i % 8 == 1:
+            t1 = 'No Emotion'
+        elif i % 8 == 2:
+            t1 = 'Anger'
+        elif i % 8 == 3:
+            t1 = 'Hate'
+        elif i % 8 == 4:
+            t1 = 'Grief'
+        elif i % 8 == 5:
+            t1 = 'P-Love'
+        elif i % 8 == 6:
+            t1 = 'R-Love'
+        elif i % 8 == 7:
+            t1 = 'Joy'
+        else:
+            t1 = 'Reverence'
+
+        if i / 8 < 1.01:
+            t2 = 'EMG(jaw)'
+        elif i / 8 < 2.01:
+            t2 = 'BVP'
+        elif i / 8 < 3.01:
+            t2 = 'GSR(palm)'
+        else:
+            t2 = 'Respiration'
+
+        list_data.append({'data':data[i-1],
+                          'emotion':t1,
+                          'day':day,
+                          'sensor':t2})
+data = [x for x in list_data if x['emotion'] == 'Anger']
+print(data)
+
+# for data in list_data:
+#     plt.plot(data[0])
+#     plt.title(data[1][0] + data[1][1] + data[1][2])
+#     plt.show()
